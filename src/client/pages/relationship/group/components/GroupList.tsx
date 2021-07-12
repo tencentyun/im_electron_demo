0098b824120075fb03f53a1fd4e3760d3bc9c123
adrien <18710735867@163.com>
2021-07-12 16:07:40 +0800

    feat: 解散群and退出群

---
+++ src/client/pages/relationship/group/components/GroupList.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import useAsyncRetryFunc from "../../../../utils/useAsyncRetryFunc";
+import { getJoinedGroupList } from "../api";
+import { GroupItem } from "./GroupItem";
+
+import "./group-list.scss";
+
+export const GroupList = (): JSX.Element => {
+  const { value, loading, retry } = useAsyncRetryFunc(async () => {
+    return await getJoinedGroupList();
+  }, []);
+
+  console.log('value', value)
+
+  return (
+    <div className="group-list">
+      {value?.map((v) => (
+        <GroupItem
+          key={v.group_base_info_group_id}
+          groupId={v.group_base_info_group_id}
+          groupAvatar={v.group_base_info_face_url}
+          groupName={v.group_base_info_group_name}
+          groupOwner={v.group_detial_info_owener_identifier}
+          onRefresh={retry}
+        />
+      ))}
+    </div>
+  );
+};
--- src/client/pages/relationship/group/index.tsx
+++ src/client/pages/relationship/group/index.tsx
@@ -1,28 +1,14 @@
 import React from "react";
-import useAsyncRetryFunc from "../../../utils/useAsyncRetryFunc";
-import { getJoinedGroupLis } from "./api";
-import { GroupItem } from "./components/GroupItem";
+import { GroupList } from "./components/GroupList";
 import { Title } from "./components/Title";
 import './index.scss';
 
 export const Group = (): JSX.Element => {
-  const { value, loading, retry } = useAsyncRetryFunc(async () => {
-    return await getJoinedGroupLis();
-  }, []);
 
   return (
     <div className="group">
       <Title />
-      <div className="group-list">
-      {value?.map((v) => (
-        <GroupItem
-          groupId={v.group_base_info_group_id}
-          groupAvatar={v.group_base_info_face_url}
-          groupName={v.group_base_info_group_name}
-        />
-      ))}
-      </div>
-     
+     <GroupList />
     </div>
   );
 };
