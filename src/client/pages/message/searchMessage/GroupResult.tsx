17cfdfd50890fb99355d3f244cb5585e89cfb224
Jingfeng <joyxian@tencent.com>
2021-07-14 12:03:08 +0800

    add some style for search modal

--- src/client/pages/message/searchMessage/GroupResult.tsx
+++ src/client/pages/message/searchMessage/GroupResult.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Avatar } from '../../../components/avatar/avatar';
+import { EmptyResult } from './EmptyResult';
 
 import './group-result.scss';
 
@@ -9,15 +10,16 @@ export const GroupResult = (props) => {
     return (
         <div className="group-result customize-scroll-style">
             {
-                result.length > 0 && result.map(item => {
+                result.length > 0 ? result.map((item, index) => {
                     const { group_detial_info_face_url,  group_detial_info_group_name } = item;
                     return (
-                        <div className="group-result__item">
+                        <div className="group-result__item" key={index}>
                             <Avatar url={group_detial_info_face_url} />
                             <span className="group-result__item--nick-name">{group_detial_info_group_name}</span>
                         </div>
                     )
-                })
+                }):
+                <EmptyResult />
             }
         </div>
     )
