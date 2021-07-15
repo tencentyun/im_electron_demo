08d5571c30e718f8cfa1ec2434f64755c8644bcb
xingchenhe(何星辰) <xingchenhe@tencent.com>
2021-07-15 16:50:13 +0800

    录音

--- src/client/pages/message/components/recordPopup.tsx
+++ src/client/pages/message/components/recordPopup.tsx
@@ -1,8 +1,13 @@
-import { List, Button } from "@tencent/tea-component"
-import React, { FC, useEffect, useRef, useState } from "react"
-import timRenderInstance from "../../../utils/timRenderInstance"
+import {  Button } from "@tencent/tea-component"
+import React, { FC, useEffect, useRef } from "react"
 import './recordPopup.scss'
-
+import Recorder from 'js-audio-recorder'
+const recorder = new Recorder({
+    sampleBits: 16,                 // 采样位数，支持 8 或 16，默认是16
+    sampleRate: 16000,              // 采样率，支持 11025、16000、22050、24000、44100、48000，根据浏览器默认值，我的chrome是48000
+    numChannels: 1,                 // 声道，支持 1 或 2， 默认是1
+    // compiling: false,(0.x版本中生效,1.x增加中)  // 是否边录边转换，默认是false
+});
 interface RecordPopupProps {
     onSend: Function
     onCancel: Function
@@ -12,6 +17,13 @@ export const RecordPopup: FC<RecordPopupProps> = ({ onSend, onCancel }): JSX.Ele
     const refPopup = useRef(null)
     
     useEffect(() => {
+        recorder.start().then(() => {
+            // 开始录音
+            console.log('开始录音')
+        }, (error) => {
+            // 出错了
+            console.log(error);
+        });
         document.addEventListener('click', handlePopupClick);
         return () => {
             document.removeEventListener('click', handlePopupClick);
