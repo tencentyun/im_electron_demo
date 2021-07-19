9f8eb06af70514491e5b1c210dc3d2d19e2670e0
lexuslin <123285068@qq.com>
2021-07-19 16:17:04 +0800

    优化代码

--- src/client/typings/index.d.ts
+++ src/client/typings/index.d.ts
@@ -141,10 +141,5 @@ declare namespace State {
         friend_profile_custom_string_info_value: string
     }
 
-    export enum TIMConvType {
-        kTIMConv_Invalid, // 无效会话
-        kTIMConv_C2C,     // 个人会话
-        kTIMConv_Group,   // 群组会话
-        kTIMConv_System,  // 系统会话
-    };
+   
 }
