cdcd42b1a1af46f60aed92ecb8168d5553d3c9a4
p_guowuliu <p_guowuliu@tencent.com>
2021-07-20 20:24:18 +0800

    同步代码

--- src/client/pages/settings/AccountSetting.tsx
+++ src/client/pages/settings/AccountSetting.tsx
@@ -17,8 +17,19 @@ export const AccountSetting = (): JSX.Element => {
         dispatch(setIsLogInAction(false));
     };
     return (
-        <div className="account-setting">
-            <Button onClick={logOutHandler}>退出登录</Button>
+        <div className="connect">
+            <header className="connect-header">
+                <span className="connect-header--logo"></span>
+                <span className="connect-header--text">账号设置</span>
+            </header>
+            <section className="connet-section">
+                {/* <div className="connect-desc">
+                    <p>
+                        简单接入，稳定必答、覆盖全球的即时通信云服务
+                    </p>
+                </div> */}
+                <Button onClick={logOutHandler} style={{ fontSize: "16px",width:"200px",height:"40px",margin: "auto",marginLeft:"30%",marginTop:"20%" }}>退出登录</Button>
+            </section>
         </div>
     )
 }
