8604cdd265a42cd57f4038f6e0d49ea4e97d5103
p_guowuliu <p_guowuliu@tencent.com>
2021-07-26 19:00:27 +0800

    代码提交0726-2

--- src/client/utils/tools.ts
+++ src/client/utils/tools.ts
@@ -44,6 +44,66 @@ const checkFileExist = (path) =>{
         params:path
     })
 }
+
+
+const throttle = (fn, delay) => {
+    let timer
+    let t_start = Date.now()
+    return function (...args) {
+        const context = this as any
+        const t_curr = Date.now()
+        clearTimeout(timer)
+        if (t_curr - t_start >= delay) {
+            fn.apply(context, args)
+            t_start = t_curr
+        } else {
+            timer = setTimeout(() => {
+                fn.apply(context, args)
+            }, delay)
+        }
+    }
+}
+
+//file对象转换为Blob对象 
+const dataURLtoBlob = (file) => {
+    return new Promise((resolve, reject) => {
+        if (!file) {
+            reject('file is null')
+        }
+        if (window.FileReader) {
+            var fr = new FileReader();
+            fr.readAsDataURL(file);
+            fr.onloadend = function (e) {
+                resolve(e.target.result)
+            }
+        } else {
+            reject('window.FileReader is undefined')
+        }
+    })
+}
+const convertBase64UrlToBlob = (urlData) => {
+    // 去掉url的头，并转换为byte
+    let bytes = window.atob(urlData.split(',')[1])
+    let ab = new ArrayBuffer(bytes.length)
+    let ia = new Uint8Array(ab)
+    for (let i = 0; i < bytes.length; i++) {
+        ia[i] = bytes.charCodeAt(i)
+    }
+    return new Blob([ab], { type: 'image/jpeg' })
+}
+
+/**
+ * 文字高亮
+ */
+const highlightText = (text, content, color = '#006eff') => {
+    if (!text) {
+      return content
+    }
+    if (!content || !content.includes(text)) {
+      return content
+    }
+    return content.replaceAll(text, `<span style='color: ${color}'>${text}</span>`)
+}
 export {
     isWin,
     minSizeWin,
