976ae5af9008f64970ae1b5be18e78aff08b36e2
weiShao <guozhao.wei@yz-intelligence.com>
2021-07-29 21:41:15 +0800

    去掉无用代码

--- src/client/pages/message/MessageInfo.tsx
+++ src/client/pages/message/MessageInfo.tsx
@@ -29,7 +29,6 @@ type Info = {
   faceUrl: string;
   nickName: string;
 };
-import ImgViewer from '../../components/ImgViewer';
 
 export const MessageInfo = (props: State.conversationItem): JSX.Element => {
   const { conv_id, conv_type, conv_profile } = props;
