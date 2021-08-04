8240d367991d1dd52a18503152fd715be41df33e
Jingfeng <joyxian@tencent.com>
2021-08-04 17:37:33 +0800

    filter type

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -323,12 +323,12 @@ export const App = () => {
             handleMessageSendFailed(convList);
             if (conversationList[0]?.conv_last_msg?.message_status === 1) {
                 const elemType = conversationList[0].conv_last_msg?.message_elem_array?.[0]?.elem_type;
-                // if(elemType === 4) {
+                if(elemType === 4 || elemType === 9) {
                     dispatch(updateMessages({
                         convId: conversationList[0].conv_id,
                         message: conversationList[0].conv_last_msg
                     }))
-                // }
+                }
             }
 
         }
