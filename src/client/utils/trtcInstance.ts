5cad00339a36b3507fd69676b76dc463707daec8
Jingfeng <joyxian@tencent.com>
2021-08-09 20:26:21 +0800

    私有化示例

--- src/client/utils/trtcInstance.ts
+++ src/client/utils/trtcInstance.ts
@@ -5,6 +5,8 @@ let trtcInstance:TRTCCloud = null;
 const getInstance = ():TRTCCloud => {
     if(!trtcInstance) {
         trtcInstance = new TRTCCloud();
+        // var config = {"proxy_env": {"sdk_appid": 1400188366, "domain": "https://common-proxy.rtc.tencent.com"}};
+        // trtcInstance.setEnvironment(JSON.stringify(config));
     }
     return trtcInstance;
 }
