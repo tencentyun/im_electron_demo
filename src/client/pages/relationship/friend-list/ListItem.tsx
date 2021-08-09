7171ffdb4a17d90ee35f2466b2619a4ad5ab79c2
adrien <18710735867@163.com>
2021-08-09 15:12:12 +0800

    feat: 好友列表删除好友

--- src/client/pages/relationship/friend-list/ListItem.tsx
+++ src/client/pages/relationship/friend-list/ListItem.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button } from "tea-component";
+import React, { useState } from "react";
+import { Button, PopConfirm } from "tea-component";
 import { Avatar } from "../../../components/avatar/avatar";
+import { deleteFriend } from "./api";
 import "./friend-list.scss";
 
 export const ListItem = (props: {
@@ -11,9 +12,17 @@ export const ListItem = (props: {
   onRefresh: () => Promise<any>;
 }) => {
   const { faceUrl, userId, userName, depName, onRefresh } = props;
+  const [deleteLoading, setDeleteLoading] = useState(false);
 
   const handleDelete = async () => {
-
+    setDeleteLoading(true);
+    try{
+      await deleteFriend(userId);
+      await onRefresh();
+    }catch(e) {
+      console.log(e);
+    }
+    setDeleteLoading(false);
   }
 
   return (
@@ -23,12 +32,33 @@ export const ListItem = (props: {
           <Avatar url={faceUrl} userID={userId} nickName={userName} />
         </div>
         <div className="item-left__info">
-          <span className="item-left__info--name">{userName}</span>
-          <span className="item-left__info--dep">{depName}</span>
+          <span className="item-left__info--name">{userName || userId}</span>
+          {/* <span className="item-left__info--dep">{depName}</span> */}
         </div>
       </div>
       <div className="item-right">
-       <Button type="link" className="item-right__btn" onClick={handleDelete}>删除</Button>
+      <PopConfirm
+            title="确认要删除该好友吗?"
+            footer={(close) => (
+              <>
+                <Button
+                  type="link"
+                  loading={deleteLoading}
+                  onClick={async () => {
+                    await handleDelete();
+                    close();
+                  }}
+                >
+                  确认
+                </Button>
+                <Button type="text" onClick={close}>
+                  取消
+                </Button>
+              </>
+            )}
+          >
+       <Button type="link" className="item-right__btn">删除</Button>
+       </PopConfirm>
       </div>
     </div>
   );
