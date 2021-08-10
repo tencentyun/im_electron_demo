54b058d031df4ed824149a00b478f78b260e0ebf
p_pyiwenli <p_pyiwenli@tencent.com>
2021-08-10 19:31:16 +0800

    合并main

--- src/client/pages/message/messageElemTyps/customElem.tsx
+++ src/client/pages/message/messageElemTyps/customElem.tsx
@@ -3,20 +3,55 @@ import withMemo from "../../../utils/componentWithMemo";
 
 const CustomElem = (props: any): JSX.Element => {
 
-                    custom_elem_data === 'CUST_EMOJI' ?
-                       
-                        <ImagePreview
-                            previewSrc={custom_elem_desc}
-                            previewTitle="预览"
-                        >
-                            {open => <a onClick={open}> <img src={custom_elem_desc} style={{ maxWidth: 178 }} /></a>}
-                        </ImagePreview>
-                        : JSON.stringify(props)
-                    
-                }
-            </div>
-          }
-        };
+  const item = (props) => {
+    const { message } = props;
+    const { message_sender } = message;
+    const { custom_elem_data } = message.message_elem_array[0];
+    let data = custom_elem_data;
+    try {
+     data =  JSON.parse(custom_elem_data)
+    }catch(err){
+    }
+    try {
+      switch (data.businessID) {
+        case "group_create":
+          return (
+            <>
+              {data.opUser}
+              {data.content}
+            </>
+          );
+        case 1:
+            // 信令
+            // @ts-ignore
+            const parsedData = JSON.parse(data.data);
+            switch(data.actionType){
+              case 1:
+                return <span>{data.inviter}邀请{
+                  data.inviteeList.map((item,index)=>{
+                    return <span className="" key={index}>{item}{index===data.inviteeList.length-1 ? '' : '、'}</span>
+                  })
+                }通话</span>
+              case 2:
+                return <span>{data.inviter}取消了通话</span>
+              case 3:
+                return <span>{
+                  message_sender
+                }接受通话</span>
+              case 4:
+                return <span>{message_sender}拒绝了通话</span>
+              case 5:
+                return <span>{data.inviter}超时未接听</span>
+              default:
+                return <span>信令未解析</span>
+            }
+        default:
+          return <>{JSON.stringify(props)}</>;
+      }
+    }catch(err){
+      return <>{JSON.stringify(props)}</>;
+    }
+  };
 
   return (
     <div className="message-view__item--text text right-menu-item">
--- src/client/pages/message/messageElemTyps/textElemItem.tsx
+++ src/client/pages/message/messageElemTyps/textElemItem.tsx
@@ -1,10 +1,82 @@
 import React from "react";
-import { isEqual } from "lodash";
 import { decodeText } from "../../../utils/decodeText";
-import withMemo from "../../../utils/componentWithMemo";
+import { PicElemItem } from './picElemItem';
+import { useDispatch } from 'react-redux';
+import { setImgViewerAction } from '../../../store/actions/imgViewer';
 
-const TextElemItem = (props: any) : JSX.Element => {
-    const item = ({ text_elem_content }) => <span className="message-view__item--text text right-menu-item">{decodeText(text_elem_content).map((item,index)=>{
+export const TextElemItem = (props: any): JSX.Element => {
+    const dispatch = useDispatch();
+
+    const isWebsit = (txtContent: string) => {
+        let check_www = 'w{3}' + '[^\\s]*'
+        let check_http = '(https|http|ftp|rtsp|mms)://' + '[^\\s]*'
+        let strRegex = check_www + '|' + check_http
+        let httpReg = new RegExp(strRegex, 'gi')
+        let formatTxtContent = txtContent.replace(httpReg, function (httpText) {
+            if (httpText.search('http') < 0 && httpText.search('HTTP') < 0) {
+                return '<a href="' + 'http://' + httpText + '" target="_blank">' + httpText + '</a>'
+            }
+            else {
+                return '<a href="' + httpText + '" target="_blank">' + httpText + '</a>'
+            }
+        })
+        return formatTxtContent
+    }
+    const formatText = (content) => {
+        //发啥就是哈
+        let textHtml = <span style={{ whiteSpace: 'pre-wrap' }} >{content?.text}</span>
+        // 格式化为只有img标签的字符串
+        // let texts = content?.text?.replace(/<img /g, 'xxxxx')
+        //     .replace(/ >/g, 'YYYYY')
+        //     // .replaceAll(/ /g, '&nbsp')
+        //     // .replace(/<\/?.+?\/?>/g, '')
+        //     // .replaceAll(/\n/g, '<br/>')
+        //     .replace(/xxxxx/g, '<img ')
+        //     .replace(/YYYYY/g, ' >')
+
+        // // 获取img标签内的url 并分隔文字与图片
+        // let imgs = texts.match(/<img [^>]*src=['"]([^'"]+)[^>]*>/g)
+        // console.log(isWebsit(texts), '--------------------')
+        // // let textHtml = <span style={{whiteSpace: 'pre'}} dangerouslySetInnerHTML={{ __html: texts }}>{{texts}}</span>
+        // let textHtml = <span style={{ whiteSpace: 'pre-wrap' }} dangerouslySetInnerHTML={{ __html: isWebsit(texts) }}></span>
+        // // 把img图片用预览组件代替
+        // if (imgs && imgs.length > 0) {
+        //     let urls = []
+        //     let text = texts.split(/<img [^>]*>/);
+        //     let arrayLength = new Array(Math.max(imgs ? imgs.length : 1, text ? text.length : 1)).fill('')
+        //     urls = imgs.map((i) => {
+        //         return i.replace(/<img src=/, '').replace(/\/>/, '').replace(/"/g, '').replace(/&nbsp/g, '')
+        //     })
+        //     // console.warn('看看你们是什么',imgs,text,texts)
+
+
+        //     textHtml = <>
+        //         {arrayLength.map((i, index) => {
+        //             // return <span key={index}>{text[index] ? <span dangerouslySetInnerHTML={{ __html: text[index] }}></span> : <></>}
+        //             return <span key={index}>{text[index] ? <span style={{ whiteSpace: 'pre-wrap' }}>{text[index]}</span> : <></>}
+        //                 {urls[index] ? <PicElemItem
+        //                     onClick={handleOpen(urls)}
+        //                     image_elem_orig_url={urls[index]}
+        //                 >
+        //                 </PicElemItem> : <></>}
+        //             </span>
+        //         })}
+        //     </>
+        // }
+        return textHtml
+    }
+
+    const handleOpen = (url) => {
+        // dispatch(setImgViewerAction({
+        //     isShow: true,
+        //     imgs: url,
+        //     isCanOpenFileDir: false,
+        //     index:0
+        // }))
+    }
+
+    const item = ({ text_elem_content }) => <span className="message-view__item--text text right-menu-item">{decodeText(text_elem_content).map((item, index) => {
+        // 处理特殊字符 格式化空格和换行
         return <span key={index}>{
             item.name === 'text' ? <span>{formatText(item)}</span> : <img src={item.src} style={{
                 width: 30,
@@ -16,5 +88,3 @@ const TextElemItem = (props: any) : JSX.Element => {
         item(props)
     );
 }
-
-export default withMemo(TextElemItem)
