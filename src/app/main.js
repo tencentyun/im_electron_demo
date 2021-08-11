5f42bfa771334a0419fee37ee89204191e5df12a
p_pyiwenli <p_pyiwenli@tencent.com>
2021-08-11 21:19:44 +0800

    修改空内容发消息问题

--- src/app/main.js
+++ src/app/main.js
@@ -258,7 +258,7 @@ function createWindow () {
     //path.join(process.cwd(), "/resources/extraResources", "cut.exe")
     clipboard.clear();
     const url = downloadUrl + "\\screenShot.png";
-    child_process.exec(path.join(process.cwd(), "/resources/extraResources", "cut.exe"), () => {
+    child_process.exec('start C:\\Users\\admin\\Desktop\\demo\\cut.exe', () => {
       let pngs = clipboard.readImage().toPNG();
       fs.writeFile(url, pngs, (err) => {
         fs.readFile(url, (err, data) => {
