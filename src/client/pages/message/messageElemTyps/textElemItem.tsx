32224e3f2b855cde19882e11c469703f744f3aaa
Jingfeng <joyxian@tencent.com>
2021-08-13 21:35:44 +0800

    文本换行

--- src/client/pages/message/messageElemTyps/textElemItem.tsx
+++ src/client/pages/message/messageElemTyps/textElemItem.tsx
@@ -3,17 +3,34 @@ import { isEqual } from "lodash";
 import { decodeText } from "../../../utils/decodeText";
 import withMemo from "../../../utils/componentWithMemo";
 
-const TextElemItem = (props: any) : JSX.Element => {
-    const item = ({ text_elem_content }) => <span className="message-view__item--text text right-menu-item">{decodeText(text_elem_content).map((item,index)=>{
-        return <span key={index}>{
-            item.name === 'text' ? <span >{item.text}</span> : <img src={item.src} style={{
-                width:30,
-                height:30
-            }}/>
-        }</span> 
-    })}</span>;
+const TextElemItem = (props: any): JSX.Element => {
+    const splitItem = props.text_elem_content.split('\n');
+    const item = (item) => (
+        <div className="message-view__item--text text right-menu-item">
+            {
+                item.map(text => {
+                    const formatedText = decodeText(text);
+                    return <div>
+                        {
+                            formatedText.map(item => {
+                                return <span>
+                                    {
+                                        item.name === 'text' ? <span>{item.text}</span>: <img src={item.src} style={{
+                                            width: 30,
+                                            height: 30
+                                        }} />
+                                    }
+                                </span>
+                            })
+                        }
+                    </div>
+                })
+            }
+        </div>
+    )
+
     return (
-        item(props)
+        item(splitItem)
     );
 }
 
