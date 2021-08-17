a1dd5c4bb8399e4f5317e2a95e36c834a88f5a6e
xingchenhe(何星辰) <xingchenhe@tencent.com>
2021-08-17 17:21:28 +0800

    32位trtc

--- src/client/pages/login/index.tsx
+++ src/client/pages/login/index.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 import { LoginAbout } from './LoginAbout';
 import { LoginContent } from './LoginContent';
 import './login.scss';
-import { Windows } from '../../components/toolsBar/windows';
+import { ToolsBar } from '../../components/toolsBar/toolsBar';
 export const Login = (): JSX.Element => (
     <div>
-        <Windows></Windows>
+        <ToolsBar ></ToolsBar>
         <div className="login">
             <LoginAbout></LoginAbout>
             <LoginContent></LoginContent>
