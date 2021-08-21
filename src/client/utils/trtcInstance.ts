5bed1d13599359957549ef5b4286690beb438333
zhaiweichao <714707947@qq.com>
2021-08-21 16:24:05 +0800

    去除公有云

--- src/client/utils/trtcInstance.ts
+++ src/client/utils/trtcInstance.ts
@@ -3,35 +3,24 @@ import getHuaRunConfig from '../constants/index'
 const { TRTC_BASE_URL, SERVERr_ADDRESS_IP} = getHuaRunConfig
 let trtcInstance: TRTCCloud = null;
 
-// const getInstance = (): TRTCCloud => {
-//     if (!trtcInstance) {
-//         trtcInstance = new TRTCCloud();
-//         // etc, 示例 config
-//         let envConfig = {};
-//         envConfig['trtc_env'] = { 
-//           'access_ip': [],
-//           'anycast_domain': '',
-//           'access_host': SERVERr_ADDRESS_IP,
-//           'access_public_key': '035D3DD10EAFE2E68942158562BB3C4266B7DCA2D3ACD7407184CE4E5F633DAFA4'
-//         }
-//         envConfig['proxy_env'] = { "sdk_appid": 1400529075, "domain":  TRTC_BASE_URL} 
+const getInstance = (): TRTCCloud => {
+    if (!trtcInstance) {
+        trtcInstance = new TRTCCloud();
+        // etc, 示例 config
+        let envConfig = {};
+        envConfig['trtc_env'] = { 
+          'access_ip': [],
+          'anycast_domain': '',
+          'access_host': SERVERr_ADDRESS_IP,
+          'access_public_key': '035D3DD10EAFE2E68942158562BB3C4266B7DCA2D3ACD7407184CE4E5F633DAFA4'
+        }
+        envConfig['proxy_env'] = { "sdk_appid": 1400529075, "domain":  TRTC_BASE_URL} 
         
-//         trtcInstance = new TRTCCloud();
-//         // etc, 示例 config
-//         trtcInstance.setEnvironment(JSON.stringify(envConfig))
-//     }
-//     return trtcInstance;
-// }
-
-
-const getInstance = (): TRTCCloud => {
-      if (!trtcInstance) {
-          trtcInstance = new TRTCCloud();
-          // etc, 示例 config
-          var config = { "proxy_env": { "sdk_appid": 1400529075, "domain":  "http://oaim.crbank.com.cn"} }
-          trtcInstance.setEnvironment(JSON.stringify(config))
-      }
-      return trtcInstance;
-  }
+        trtcInstance = new TRTCCloud();
+        // etc, 示例 config
+        trtcInstance.setEnvironment(JSON.stringify(envConfig))
+    }
+    return trtcInstance;
+}
 
 export default getInstance();
