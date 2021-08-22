9493f7c294a05b3da91d08c29e979500c5bfb14a
Jingfeng <joyxian@tencent.com>
2021-08-22 12:22:09 +0800

    解决冲突

--- src/client/store/reucers/historyMessage.ts
+++ src/client/store/reucers/historyMessage.ts
@@ -39,11 +39,7 @@ const messageReducer = (state = initState, action: Action): State.historyMessage
     }
     case ActionTypeEnum.RECI_MESSAGE: {
       const history = state.historyMessageList.get(payload.convId);
-<<<<<<< HEAD
       const baseTime = getBaseTime(history);
-=======
-      const baseTime =  getBaseTime(history);
->>>>>>> origin
       const timeDividerResult = addTimeDivider(payload.messages, baseTime).reverse();
        
       const ret = {
