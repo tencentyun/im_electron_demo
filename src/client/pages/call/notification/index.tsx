5f37c2de92f016951035248f1ac9e60117471ff5
Jingfeng <joyxian@tencent.com>
2021-08-24 11:14:00 +0800

    音视频

--- src/client/pages/call/notification/index.tsx
+++ src/client/pages/call/notification/index.tsx
@@ -16,17 +16,19 @@ export const Notification = (props) => {
 
     const getDisplayText = () => `邀请你进行${isVoiceCall ? '语音' : '视频'}通话`;
 
+    const closeCallWIndow = () => {
+        const win = remote.getCurrentWindow();
+        win.close();
+    }
+
     useEffect(()=>{
-        console.log('注册退出事件')
         event.on('exitRoom',()=>{
             // 如果没有接通，走这个退出逻辑
-            endCallWindow()
-        })
+            closeCallWIndow();
+        });
 
         let timer = setTimeout(() => {
-            // endCallWindow();
-            const win = remote.getCurrentWindow();
-            win.close();
+            closeCallWIndow();
         }, 30 * 1000);
 
         return () => {
