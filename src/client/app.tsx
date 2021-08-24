7b487d56eedbb25371c839e09b400ef8a0d6c561
Jingfeng <joyxian@tencent.com>
2021-08-24 17:07:28 +0800

    音视频放开自定义消息

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -502,7 +502,7 @@ export const App = () => {
             }
             // if (conversationList[0]?.conv_last_msg?.message_status === 1) {
                 const elemType = conversationList[0].conv_last_msg?.message_elem_array?.[0]?.elem_type;
-                if (elemType === 4 || elemType === 9) {
+                if (elemType === 4 || elemType === 9 || elemType === 3) {
                     dispatch(updateMessages({
                         convId: conversationList[0].conv_id,
                         message: conversationList[0].conv_last_msg
