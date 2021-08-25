6320405789af2ac6a0d678c7c3b5a9600b29660e
p_guowuliu <p_guowuliu@tencent.com>
2021-08-26 00:55:22 +0800

    :配置改了，回退一下

--- src/client/constants/index.ts
+++ src/client/constants/index.ts
@@ -132,7 +132,7 @@ type HuaRunConfig = {
     HUA_RUN_SYSTEMID: string
 }
 const getHuaRunConfig = (): HuaRunConfig => {
-    const env = 'dev';
+    const env = getParamsByKey('HUARUN_ENV');
     console.log('环境变量',env )
     return HUARUN_CONFIG[env]
 }
