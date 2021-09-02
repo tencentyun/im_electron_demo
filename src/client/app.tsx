c72e315a851122df39379b0fee8685018c5215ee
p_guowuliu <p_guowuliu@tencent.com>
2021-09-02 12:04:45 +0800

    :消息免打扰失效，登录后如果本地缓存没有值默认设置true

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -211,7 +211,7 @@ export const App = () => {
         console.log(showApp, '[[[[[[[[[[[[[[[', msgBother)
         console.log(messages)
         // 客户端没有展示在最顶层或者设置了消息提示免打扰，就不接收消息通知
-        if (showApp || msgBother == 'false') {
+        if (showApp || msgBother == 'true') {
             return;
         }
         console.log(messages[0].message_elem_array[0], '通知消息------------------------------------', messages)
