a2910cc071d3c032a293b2e51ab3d9aaa7ba37bf
ZhaiWeiChao <31640163+zhaiweichao@users.noreply.github.com>
2021-09-03 10:44:17 +0800

    Main730 zhaiweichao (#117)
    修改UI

--- src/client/pages/message/searchMessage/SearchMessage.tsx
+++ src/client/pages/message/searchMessage/SearchMessage.tsx
@@ -134,7 +134,7 @@ export const SearchMessage = (props) => {
         <div className="search-message">
             <section className="search-message__input-area">
                 <Icon className="search-message__input-area--icon" type="search" />
-                <Input className="search-message__input-area--input" type="search" placeholder="查找消息等" onChange={handleInoputOnchange} />
+                <Input className="search-message__input-area--input" type="search" autoFocus placeholder="查找消息等" onChange={handleInoputOnchange} />
                 <Icon className="search-message__input-area--icon-close" type="close" onClick={handleModalClose} />
             </section>
             <section className="search-message__tab">
